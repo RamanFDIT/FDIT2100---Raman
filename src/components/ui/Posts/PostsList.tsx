b349c40b8a295622210deb010cea4d1458455949
Ramandeep Singh <rs9896158641@gmail.com>
2025-09-25 10:27:29 -0700

    swith to use SuspenseInfinite query for paging

--- src/components/ui/Posts/PostsList.tsx
+++ src/components/ui/Posts/PostsList.tsx
@@ -1,18 +1,43 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { fetchPosts } from "@/lib/api";
 import Post from "@/components/ui/Posts/post";
+import  {ArrowRightIcon} from "@heroicons/react/24/solid";
+import { Button } from "@/components/ui/button";
+import styles from './postsList.module.css';
 
 export default function PostsList() {
-  const { data } = useSuspenseQuery({
+  const [page, setPage] = useState(0);
+
+  const { 
+    data,
+    fetchNextPage,
+    hasNextPage,
+  } = useSuspenseInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryFn:  ({pageParam}) => fetchPosts(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (mostRecentPage) => {
+      return mostRecentPage.skip + mostRecentPage.limit < mostRecentPage.total ? page + 1 : undefined;
+    }
   });
   console.log('data', data);
   return (
+    <>
       <ul>
-        {data.posts.map((post, index) => (
-          <Post key={post.id} post={post} isLast={index === data.posts.length - 1}/>
-        ))}
+        {data.pages.map((page, pageIndex) => {
+          return page.posts.map((post, postIndex) => (
+              <Post key={post.id} post={post} isLast={postIndex === page.posts.length - 1 && pageIndex === data.pages.length - 1} />
+          ));
+        })}
       </ul>
+      {hasNextPage && <div className="flex justify-end">
+        <Button variant="link" className={styles.loadMoreButton} onClick={( ) => {
+          setPage (page => page + 1);
+          fetchNextPage();
+        }}>Load More <ArrowRightIcon/> </Button>
+        </div>
+      }
+    </>  
   );
 }  
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { type PostResponse } from '@/lib/types/post';
 import { API_BASE_URL } from '@/lib/constants';
+import { POSTS_PER_PAGE } from '@/lib/constants';
 
-export function fetchPosts() {
-    return axios.get<PostResponse>(`${API_BASE_URL}/posts`)
+export function fetchPosts(page: number) {
+    return axios.get<PostResponse>(`${API_BASE_URL}/posts?limit=${POSTS_PER_PAGE}&skip=${page * POSTS_PER_PAGE}`)
         .then(response => response.data);
 }
